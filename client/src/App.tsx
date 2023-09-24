4277872b3697a06ceceb26f175ba76ae0a5ca0c0
dev-newton <newtonimonjirie@gmail.com>
2023-09-24 14:51:23 +0100

    feat: add footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 /** @format */
 
+import { Route, Routes } from 'react-router-dom';
+
+import Footer from 'components/Footer/Footer';
 import Navbar from 'components/Navbar/Navbar';
+import Home from 'pages/Home/Home';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
