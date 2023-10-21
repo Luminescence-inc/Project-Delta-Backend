441725cee23ef8c174244de9191d7150cb3334ea
walterokpala <uchewalter8@gmail.com>
2023-10-20 21:34:04 -0400

    removed dummy test files

--- server/src/__test__/add.test.ts
+++
@@ -1,8 +0,0 @@
-import add from '../math/add';
-
-describe('This is a test', () => {
-  it('should pass', () => {
-    const result = add(2, 3);
-    expect(result).toBe(5);
-  });
-});
--- server/src/index.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv-safe';
-import add from './math/add';
-dotenv.config();
-console.log('Hi');
-
-console.log(process.env.MY_NAME);
-console.log(add(2, 3));
