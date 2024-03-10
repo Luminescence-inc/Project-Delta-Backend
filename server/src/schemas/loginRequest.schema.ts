b9b579922ee07342b720f4aa18bedcdf6457c12f
walterokpala <uchewalter8@gmail.com>
2024-03-10 03:16:09 -0400

    removed all

--- server/src/schemas/loginRequest.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { RequestSchema } from './request.schema.js';
-
-export const LoginRequestSchema = RequestSchema.extend({
-  body: z.object({
-    email: z.string(),
-    password: z.string(),
-  }),
-});
