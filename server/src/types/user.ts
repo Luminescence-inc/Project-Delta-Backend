ca195de8418aaa531ad3487a82697a29319c09df
walterokpala <uchewalter8@gmail.com>
2024-04-27 15:23:58 -0400

    created the logic for get user to send emails

---
+++ server/src/types/user.ts
@@ -0,0 +1,13 @@
+export interface UserDetailsForEmail {
+  uuid: string;
+  email?: string;
+}
+
+export interface ReminderLogDetails {
+  uuid: string;
+  userUuid: string;
+  numberOfTimesSent?: number;
+  modifiedUtc?: Date;
+  createdUtc?: Date;
+  emailType?: 'VERIFY_EMAIL' | 'CREATE_PROFILE';
+}
