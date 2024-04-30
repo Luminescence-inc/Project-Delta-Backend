42f3f2e4b5d5af492ebcc3ce14fb1957ae30a4b0
Oscar Waka <oscar.waka@orkestrascs.com>
2024-04-30 17:04:44 -0400

    TEC-101-send-grid-integration

--- server/src/utils/email.util.ts
+++ server/src/utils/email.util.ts
@@ -1,9 +1,13 @@
 import nodemailer from 'nodemailer';
 import handlebars from 'handlebars';
+import sendgrid from '@sendgrid/mail';
 import fs from 'fs';
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 const contactSupportTemplateSource = fs.readFileSync('src/templates/contact_support_template.html', 'utf-8').toString();
 const clientBaseUrl = process.env.CLIENT_BASE_URL;
+
 const source = fs.readFileSync('src/templates/email_template.html', 'utf-8').toString();
 const template = handlebars.compile(source);
 
@@ -15,32 +19,53 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// export const generateVerificationEmail = (
+//   userId: string,
+//   userEmail: string,
+//   uniqueString: string,
+//   expiresUtc: number
+// ) => {
+//   const date: any = new Date(expiresUtc);
+//   const currentTime: any = new Date();
+//   const replacements = {
+//     title: 'Account Verification Email',
+//     content: `Please click on the link below to verify your account and complete registration. Link will expire in ${Math.round(
+//       (date - currentTime) / (1000 * 60 * 60)
+//     )} hours`,
+//     link: `${clientBaseUrl + '/verify-email/' + userId + '/' + uniqueString}`,
+//   };
+//   const htmlToSend = template(replacements);
+//   const mailOptions = {
+//     from: process.env.AUTH_EMAIL,
+//     to: userEmail,
+//     subject: 'Verify Your Email',
+//     text: 'Verification Email',
+//     html: htmlToSend,
+//   };
+
+//   return transporter.sendMail(mailOptions);
+// };
+
 export const generateVerificationEmail = (
   userId: string,
   userEmail: string,
   uniqueString: string,
   expiresUtc: number
 ) => {
-  const date: any = new Date(expiresUtc);
-  const currentTime: any = new Date();
-  const replacements = {
-    title: 'Account Verification Email',
-    content: `Please click on the link below to verify your account and complete registration. Link will expire in ${Math.round(
-      (date - currentTime) / (1000 * 60 * 60)
-    )} hours`,
-    link: `${clientBaseUrl + '/verify-email/' + userId + '/' + uniqueString}`,
-  };
-  const htmlToSend = template(replacements);
-  const mailOptions = {
-    from: process.env.AUTH_EMAIL,
-    to: userEmail,
-    subject: 'Verify Your Email',
-    text: 'Verification Email',
-    html: htmlToSend,
-  };
 
-  return transporter.sendMail(mailOptions);
-};
+  return sendgrid.send({
+    from: 'BizConnect24 <noreply@bizconnect24.com>',
+    templateId: 'd-91087e5e16dc4548a7e85c769b79fcff',
+    personalizations:[
+      {
+        to: `${userEmail}`,
+        dynamicTemplateData: {
+          description: 'Dele is on the way!',
+        },
+      } 
+     ],
+  });
+}
 
 export const generateForgotPasswordEmail = (
   userId: string,
