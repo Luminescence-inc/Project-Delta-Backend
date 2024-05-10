6704989a2afbb16b9ffe65fed776a71d34935a56
walterokpala <uchewalter8@gmail.com>
2024-05-10 18:58:49 -0400

    Removed unsused code

--- server/src/utils/email.util.ts
+++ server/src/utils/email.util.ts
@@ -2,8 +2,6 @@ import nodemailer from 'nodemailer';
 import handlebars from 'handlebars';
 import sendgrid from '@sendgrid/mail';
 import fs from 'fs';
-import { VerifyEmailData } from './reminder.utils';
-import { EmailType } from '@prisma/client';
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string);
 
