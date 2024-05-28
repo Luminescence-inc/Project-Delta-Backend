c4dfd089eef0057a7a447bd929663005f5981607
benrobo <alumonabenaiah71@gmail.com>
2024-05-28 20:08:18 +0100

    refactor: Remove server.ts and update server routing to use array of routes

--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import App from './app';
-import UserRoute from './routes/user.route';
-import BusinessRoute from './routes/business.route';
-
-const server = new App();
-
-server.initializedRoutes([UserRoute, BusinessRoute]);
-server.listen();
